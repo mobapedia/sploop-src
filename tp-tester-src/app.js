d9d8f4957f1f82e08b6547477bb956579df23464
mobapedia <93173949+mobapedia@users.noreply.github.com>
2026-05-21 17:53:08 -0600

    Update app.js

--- tp-tester-src/app.js
+++ tp-tester-src/app.js
@@ -6579,13 +6579,13 @@ function i(t) {
 //edit
             T = new Proxy(T, {
               set(target, prop, value) {
-                if (prop !== 'length') {
-                  if (typeof value.Oh === "string") {
-                    if (value.Oh !== "info-panel-holder" && value.Oh !== "dist" && value.Oh !== "localStorage" && value.Oh !== "WebSocket" && value.Oh !== "webdriver" && value.Oh !== "Error" && value.Oh !== "stack" && value.Oh !== "includes" && value.Oh !== "userscript" && value.Oh !== "Function" && value.Oh !== "prototype" && value.Oh !== "toString") {
-                      console.log(`T[${prop}]`, value);
+                //if (prop !== 'length') {
+                 // if (typeof value.Oh === "string") {
+                    //if (value.Oh !== "info-panel-holder" && value.Oh !== "dist" && value.Oh !== "localStorage" && value.Oh !== "WebSocket" && value.Oh !== "webdriver" && value.Oh !== "Error" && value.Oh !== "stack" && value.Oh !== "includes" && value.Oh !== "userscript" && value.Oh !== "Function" && value.Oh !== "prototype" && value.Oh !== "toString") {
+                      console.log(T);
                       //debugger;
-                    }
-                  }
+                    //}
+                 // }
                 }
                 target[prop] = value;
                 return true;
