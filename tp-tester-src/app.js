c20bec7a5330b876a2d2877d409ef6ce65898b4d
mobapedia <93173949+mobapedia@users.noreply.github.com>
2026-06-10 19:12:57 -0600

    Update app.js

--- tp-tester-src/app.js
+++ tp-tester-src/app.js
@@ -11468,10 +11468,10 @@ b = origB.map((h, op) => function() {
           C(e);
         } else {
           z(zo[n], e, zo[n + 1], zo[n + 8], zo[n + 4] | zo[n + 5] << 8, zo[n + 6] | zo[n + 7] << 8, b().Pf(zo[n + 9]), o, zo[n + 11], zo[n + 12], zo[n + 13], zo[n + 14], zo[n + 15], zo[n + 16], zo[n + 17], zo[n + 18], t);
-          if (zo[n] === 19) {
+          if (zo[n] === 21) {
             const x = zo[n + 4] | zo[n + 5] << 8;
             const y = zo[n + 6] | zo[n + 7] << 8;
-            console.log("TREE FARM at: "+x+", "+y)
+            console.log("STONE FARM at: "+x+", "+y)
           }
         }
       }
