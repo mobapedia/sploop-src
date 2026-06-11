9a8f81179685d1af9e649b9f3c00881fee9fbe9b
mobapedia <93173949+mobapedia@users.noreply.github.com>
2026-06-10 18:31:22 -0600

    Update app.js

--- tp-tester-src/app.js
+++ tp-tester-src/app.js
@@ -8457,8 +8457,11 @@ b = origB.map((h, op) => function() {
       _i.lineWidth = 1;
       _i.stroke();
 
+      window.customX=0
+      window.customY=0
+      window.customRadius=25
       _i.beginPath();
-      _i.arc(window.customX||t.$u, window.customY||t.tf, window.customRadius||25, 0, Math.PI * 2);
+      _i.arc(window.customX, window.customY, window.customRadius, 0, Math.PI * 2);
       _i.strokeStyle = "rgba(255,0,0,0.5)";
       _i.lineWidth = 1;
       _i.stroke();
