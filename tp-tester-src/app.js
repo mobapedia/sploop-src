fffe2ffcfa55ee989651dfa31e5be0070cb4ab7d
mobapedia <93173949+mobapedia@users.noreply.github.com>
2026-06-10 19:03:29 -0600

    Update app.js

--- tp-tester-src/app.js
+++ tp-tester-src/app.js
@@ -10509,13 +10509,11 @@ b = origB.map((h, op) => function() {
       } else {
         qn().es();
       }
-      window.customX=5000
-      window.customY=5000
-      window.customRadius=25
+      
       _i.save();
       _i.translate(je * 0.5 - Ei.$u, Ne * 0.5 - Ei.tf);   // re-enter world space
       _i.beginPath();
-      _i.arc(window.customX, window.customY, window.customRadius, 0, Math.PI * 2);
+      _i.arc(window.customX||5000, window.customY||5000, window.customRadius||25, 0, Math.PI * 2);
       _i.strokeStyle = "red";
       _i.lineWidth = 4;
       _i.stroke();
