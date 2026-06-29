3c269cc4ed2678b388ecb56d7e60da89ef3e1e0a
mobapedia <93173949+mobapedia@users.noreply.github.com>
2026-06-29 07:53:44 -0700

    Update bettersploop.js

--- custom/bettersploop.js
+++ custom/bettersploop.js
@@ -9231,7 +9231,7 @@ const radiusMap = {
         
         if (window.globalSettings.coords.enabled) {
           //map margins are 5px
-          const map = Z[P().x]
+          const map = Z[P().vi]
           const mapTop = Si-map.ol.sl-5;
           const mapWidth = map.ol.al;
           
