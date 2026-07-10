ead7351d2fa1d30fbbefe58379c85f1e7fa67036
mobapedia <93173949+mobapedia@users.noreply.github.com>
2026-07-10 08:17:44 -0600

    Update bettersploop.js

--- custom/bettersploop.js
+++ custom/bettersploop.js
@@ -10347,7 +10347,7 @@ function getFittedCircleCenter(c1, c2, rNew = 35) {
             if (max > 0 && window.globalSettings.itemCounter.enabled) {
                 n.save();
                 n.font = "17px Baloo Paaji";
-                n.textAlign = "middle";
+                n.textAlign = "center";
                 n.textBaseline = "bottom";
                 n.lineWidth = 4;
                 n.strokeStyle = "#000";
