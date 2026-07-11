501f8a4310fe77cd84cad0f30a583bed3fe8fa43
mobapedia <93173949+mobapedia@users.noreply.github.com>
2026-07-10 20:08:44 -0600

    Update bettersploop.js

--- custom/bettersploop.js
+++ custom/bettersploop.js
@@ -10352,8 +10352,8 @@ function getFittedCircleCenter(c1, c2, rNew = 35) {
                 n.lineWidth = 4;
                 n.strokeStyle = "#000";
                 n.fillStyle = have >= max ? "red" : "#fff";
-                const x = t.zh/* + t.width - 4*/;
-                const y = t.Mh/* + t.height - 4*/;
+                const x = t.zh+t.width/2/* + t.width - 4*/;
+                const y = t.Mh+t.height-5/* + t.height - 4*/;
                 n.strokeText(text, x, y);
                 n.fillText(text, x, y);
 
